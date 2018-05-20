cbf5f32e36ca6500ef492e5ee7dff2eee561e9e7
buzzkillb <travanx@gmail.com>
2018-05-20 11:30:25 -0700

    added menu

---
+++ egemnode.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+while true
+do
+  clear
+  echo "=========================="
+  echo "Egem Node Installer v1.337"
+  echo "=========================="
+  echo "Enter 1 to Install Egem Node with Swap File: "
+  echo "Enter 2 to Install Egem Node without Swap File: "
+  echo "Enter q to exit this menu: "
+  echo -e "\n"
+  echo -e "Enter your selection \c"
+  read answer
+  case "$answer" in
+    1) bash -c "$(wget -O - https://raw.githubusercontent.com/buzzkillb/EGEM-node-install/master/createswap.sh)" && bash -c "$(wget -O - https://raw.githubusercontent.com/buzzkillb/EGEM-node-install/master/setup.sh)" ;;
+    2) bash -c "$(wget -O - https://raw.githubusercontent.com/buzzkillb/EGEM-node-install/master/setup.sh)" ;;
+    q) exit ;;
+  esac
+  echo -e "Enter return to continue \c"
+  read input
+done
